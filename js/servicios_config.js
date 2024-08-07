2f2a78a87aa57b4827d2eab23cc0965719195d0b
keithcampos02 <alonso826campos@gmail.com>
2024-08-06 20:01:14 -0600

    entregable 2 8/6/2024

---
+++ js/servicios_config.js
@@ -0,0 +1,27 @@
+export const ObtenerDatosDeConfiguracion = async () => {
+
+
+let url = "/js/Configuraciones.json"
+
+
+    try {
+
+        const response = await fetch(url);
+if (response.status == "200") {
+    const data = await response.json();
+
+        console.log(data);
+
+    
+}else{
+    console.log("error al consultar el api"+response.status)
+}
+
+        
+    } catch (error) {
+
+        console.log("error en la red"+ error);
+
+    }
+
+};
